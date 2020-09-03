cfa0f5e49123ced259e218e4681de54ab86bc003
ricardowoj <ricardowoj@hotmail.com>
2020-09-03 19:00:18 -0300

    create app.js with vuejs

---
+++ app.js
@@ -0,0 +1,18 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    products: [],
+  },
+  methods: {
+    fetchProducts() {
+      fetch("./api/products.json")
+        .then((r) => r.json())
+        .then((r) => {
+          this.products = r;
+        });
+    },
+  },
+  created() {
+    this.fetchProducts();
+  },
+});
