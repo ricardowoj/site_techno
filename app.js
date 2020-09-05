84ff0a15f68211568fe39b5dc8f10cefdb4254e5
ricardowoj <ricardowoj@hotmail.com>
2020-09-04 22:55:14 -0300

    edit app.css: create fetchProtudo

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const vm = new Vue({
   el: "#app",
+
   data: {
     produtos: [],
+    produto: false,
+    carrinhoTotal: 0,
+    carrinho: [],
   },
+
   methods: {
     fetchProdutos() {
       fetch("./api/produtos.json")
@@ -11,13 +16,30 @@ const vm = new Vue({
           this.produtos = r;
         });
     },
+
+    fetchProduto(nome) {
+      fetch(`./api/produtos/${nome}/dados.json`)
+        .then((r) => r.json())
+        .then((r) => {
+          this.produto = r;
+        });
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+
+    adicionarItem() {
+      this.produto.estoque--;
+    },
   },
+
   created() {
     this.fetchProdutos();
   },
+
   filters: {
     numeroPreco(valor) {
-      console.log(valor);
       return valor.toLocaleString("pt-BR", {
         style: "currency",
         currency: "BRL",
